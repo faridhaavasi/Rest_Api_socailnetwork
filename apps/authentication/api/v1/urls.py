1c06483a936ed23971409e8aaab0ec08e7602456
faridhaavasi <faridhavasi777@gmail.com>
2025-03-09 16:03:43 +0330

    add JWT token endpoints for authentication

--- apps/authentication/api/v1/urls.py
+++ apps/authentication/api/v1/urls.py
@@ -5,8 +5,15 @@ from apps.authentication.api.v1.views.register_signup_views import (
     SetPasswordApiView,
 )
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('setemail', SeteEailApiView.as_view(), name='set_email'),
     path('confirmemail/<str:token>', ConfirmEmailApiView.as_view(), name='confirm_email'),
     path('setpassword', SetPasswordApiView.as_view(), name='set_password'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
