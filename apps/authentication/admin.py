9ed1dec2f78dd5703bdfc9db85f6085607fa63c1
faridhaavasi <faridhavasi777@gmail.com>
2025-03-12 15:40:08 +0330

    model token add

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import TokenModel
 
 # Register your models here.
+
+admin.site.register(TokenModel)
--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class TokenModel(models.Model):
+    token = models.CharField()
+    value = models.CharField()
+
+
+
+        
