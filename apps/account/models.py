e2076821e951a8b8bd8f68fb2adaad44cc92a899
faridhaavasi <faridhavasi777@gmail.com>
2025-03-16 17:13:14 +0330

    feat: add AccountModel with avatar and bio fields, and register in admin

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from apps.users.models import User
 # Create your models here.
+
+
+
+class AccountModel(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account')
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
+    
