227ff09a5f0976d161f4c6661e31f59176a3e975
faridhaavasi <faridhavasi777@gmail.com>
2025-03-28 16:56:07 +0330

    feat: add Post model with admin configuration and initial migration

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from apps.posts.models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'is_published')
+    list_filter = ('is_published', 'created_at')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'created_at'
+    ordering = ('-created_at',)
+    list_editable = ('is_published',)
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'slug', 'image', 'content', 'author')
+        }),
+        ('Publishing Options', {
+            'fields': ('is_published', 'published_at')
+        }),
+    )
+    readonly_fields = ('published_at',)
+
+
+admin.site.register(Post, PostAdmin)
--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model # Import the custom user model
+from django.utils import timezone
+from django.utils.text import slugify
 
-# Create your models here.
+
+user = get_user_model() # Custom user model global variable
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    image = models.ImageField(upload_to='posts/images/', null=True, blank=True)
+    content = models.TextField()
+    author = models.ForeignKey(user, on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    published_at = models.DateTimeField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+    def publish(self):
+        self.is_published = True
+        self.published_at = timezone.datetime.now()
+        self.save()
+
+    def __str__(self):
+        return f'{self.title} by {self.author.email}'
