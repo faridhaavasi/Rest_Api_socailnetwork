00361820682f2a09dd8f5954f736253185166936
faridhaavasi <faridhavasi777@gmail.com>
2025-04-23 18:03:58 +0330

    feat: add Comment model and admin interface for managing comments

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -32,3 +32,15 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.title} by {self.author.email}'
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    replay = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+    author = models.ForeignKey(user, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.email} on {self.post.title}' 
