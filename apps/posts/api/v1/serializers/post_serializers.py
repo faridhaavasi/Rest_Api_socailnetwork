43243931d417ebaa61daf5b0943b4fbfd44da535
faridhaavasi <faridhavasi777@gmail.com>
2025-04-24 17:48:44 +0330

    feat: add Comment serializers for input and output handling

--- apps/posts/api/v1/serializers/post_serializers.py
+++ apps/posts/api/v1/serializers/post_serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from apps.posts.models import Post
+from apps.posts.models import Post, Comment
 
 
 class InputPostSerializer(serializers.ModelSerializer):
@@ -18,14 +18,52 @@ class InputPostSerializer(serializers.ModelSerializer):
 
 
 class OutputPostSerializer(serializers.ModelSerializer):
+    comments = serializers.SerializerMethodField()
+    def get_comments(self, obj):
+        comments = obj.comments.all()
+        return OutputCommentSerializer(comments, many=True).data
     class Meta:
         model = Post
         fields = ['id', 'title', 'slug', 
                 'image', 'content', 
                 'author', 'created_at',
                 'updated_at', 'is_published',
-                'published_at'
+                'published_at', 'comments'
+                ]
 
-               ] 
+             
           
+        
+class InputCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['post', 'replay', 'author', 'content']
+        read_only_fields = ['author']
+        extra_kwargs = {
+            'post': {'required': True},
+            'replay': {'required': False},
+            'author': {'required': False},
+            'content': {'required': True},
+        }
+
+class OutputCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'replay', 'author', 'content', 'created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at']
+        extra_kwargs = {
+            'post': {'required': True},
+            'replay': {'required': False},
+            'author': {'required': False},
+            'content': {'required': True},
+        }
+
+class ReplayCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['replay']
+        read_only_fields = ['replay']
+        extra_kwargs = {
+            'replay': {'required': True},
+        }
         
